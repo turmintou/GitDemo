003eaae8138e8d64a7ed525dd6a7910910db79fe
turmintou <turmintou@gmail.com>
2011-07-19 01:14:41 +0800

    add TestDepot project

---
+++ TestDepot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestDepot::Application.config.secret_token = '031ce022680c180b32550fb874a847dbb4de8ecc8f2139e5b856583f63d9d72c9a2d30064a803ec4cbcab45556eb25c1f72e67bd1bf6fd16d25d1bf44edcf59b'
